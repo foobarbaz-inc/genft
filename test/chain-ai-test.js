97a9b623161c3861e2d4320fcf956da811f70a48
Samantha Whitmore <whitmore.samantha@gmail.com>
2022-03-04 16:36:01 -0800

    add last test case

--- test/chain-ai-test.js
+++ test/chain-ai-test.js
@@ -75,7 +75,20 @@ describe("ChainAI", function () {
   });
   // TESTING ARTNFT BASIC FUNCTIONS
   it("Initializes the ArtNft contract only once", async function () {
-
+    const { chainAI, deployer, sequencer, randomPerson } = await deployChainAI(0, 0);
+    await chainAI.connect(deployer).addSequencer(sequencer.address)
+    const artNft = await deployArtNFT()
+    const genft = await deployGENft("", artNft.address, chainAI.address, 0)
+    var blockNumber = await ethers.provider.getBlockNumber();
+    var timestamp = (await ethers.provider.getBlock(blockNumber)).timestamp + 1;
+    expect(await genft.mint(randomPerson.address, ""))
+      .to.emit(genft, "Transfer").withArgs(
+        '0x0000000000000000000000000000000000000000', randomPerson.address, 1)
+      .to.emit(chainAI, "JobCreated").withArgs(1, 0, "", "", timestamp)
+    var childContract = await genft.tokenIdToChildContract(1);
+    var childArtNft = new ethers.Contract(childContract, artNft.interface, deployer);
+    await expect(childArtNft.connect(deployer).initialize(deployer.address, deployer.address, deployer.address, 0))
+      .to.be.revertedWith("Already initialized")
   });
   it("Creates inference job & allows the ML coordinator to set the tokenURI", async function () {
     const { chainAI, deployer, sequencer, randomPerson } = await deployChainAI(0, 0);
@@ -126,7 +139,7 @@ describe("ChainAI", function () {
         '0x0000000000000000000000000000000000000000', randomPerson.address, 1)
       .to.emit(chainAI, "JobCreated").withArgs(1, 0, "", "", timestamp)
     expect(await chainAI.connect(sequencer).updateJobStatus(1, 2, "http://foo"))
-      .to.emit(genft, "TokenUriSet", 1)
+      .to.emit(genft, "TokenUriSet", 1, "http://foo")
       .to.emit(chainAI, "JobSucceeded", 1)
   });
 });
